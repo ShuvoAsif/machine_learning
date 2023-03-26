aca32399a048a76a2d1a2e6d9c0f7054638e5f70
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-26 22:00:09 +0600

    data

--- extract_data.py
+++ extract_data.py
@@ -1,11 +1,22 @@
 # pip install requests
 # pip install beautifulsoup4 lxml
+# [1::1] means start for index1
 import requests
 from bs4 import BeautifulSoup
 url = 'https://en.wikipedia.org/wiki/IPhone'
 res = requests.get(url).text
 soup = BeautifulSoup(res, 'lxml')
-table = soup.find('table', class_='wikitable')
-# [1::1] means start for index1
+table = soup.find('table', class_="wikitable")
+# print(table)
 rows = table.find_all('tr')[1::1]
-print(table)
+
+for row in rows:
+    data = row.find_all(['th', 'td'])
+
+    try:
+        version_text = data[0].a.text.split('/')[0]
+        version_text = ''.join(i for i in version_text if i.isdigit())
+        version = int(version_text)
+        print(version)
+    except:
+        pass
