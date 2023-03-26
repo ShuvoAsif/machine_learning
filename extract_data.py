7b45dac09c87785ca2cff444d3ed9086f6e83ed3
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-26 21:15:04 +0600

    beautifulsoup lxml

--- extract_data.py
+++ extract_data.py
@@ -1,5 +1,11 @@
 # pip install requests
+# pip install beautifulsoup4 lxml
 import requests
+from bs4 import BeautifulSoup
 url = 'https://en.wikipedia.org/wiki/IPhone'
 res = requests.get(url).text
-print(res)
+soup = BeautifulSoup(res, 'lxml')
+table = soup.find('table', class_='wikitable')
+# [1::1] means start for index1
+rows = table.find_all('tr')[1::1]
+print(table)
