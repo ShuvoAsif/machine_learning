e1a5319da7aaeed4618835569f61607361d34338
ShuvoAsif <asif.shuvo.learning@gmail.com>
2023-03-27 17:24:56 +0600

    price

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-# pip install requests
-# pip install beautifulsoup4 lxml
-# [1::1] means start for index1
-# python pandas
-# python mapplotlib
-# scikit-learn
 import pandas
 from sklearn.linear_model import LinearRegression
 data = pandas.read_csv('iphone_price.csv')
 model = LinearRegression()
 model.fit(data[['version']], data[['price']])
 print(model.predict([[20]]))
+print(model.predict([[25]]))
+print(model.predict([[30]]))
